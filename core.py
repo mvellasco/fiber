771fbca1685c74c9aa01f858e98487e8ce284857
Miguel Vellasco <miguelvellasco@gmail.com>
2024-02-09 19:02:36 -0300

    ref(fiber): refactoring and adjusting the code all around

--- core.py
+++ core.py
@@ -1,130 +1,131 @@
-import uuid
+from datetime import datetime
 
 import falcon
-from falcon.media.validators import jsonschema
 
 from pony.orm import (
     Database,
     db_session,
+    commit,
     PrimaryKey,
     Required,
     Optional,
-    StrArray,
+    Set,
     select,
+    OptimisticCheckError,
+    TransactionIntegrityError,
 )
 
 
-# db = Database()
-# db.bind(provider="postgres", user="postgres", password="postgres", host="db", database="banco")
+db = Database()
+db.bind(provider="postgres", user="postgres", password="postgres", host="db", database="banco")
 
 
-# person_schema = {
-#     "type": "object",
-#     "properties": {
-#         "apelido": {"type": "string"},
-#         "nome": {"type": "string"},
-#         "stack": {"type": "array", "items": {"type": "string"}}
-#     }
-# }
+class Client(db.Entity):
+    id = PrimaryKey(int, auto=False)
+    limit = Required(int)
+    balance = Required(int)
+    transactions = Set("Transaction", reverse="id_do_cliente")
+
+    def last_10_transactions(self):
+        return Transaction.select(id_do_cliente=self.id)[:10]
 
 
-# class Person(db.Entity):
-#     id = PrimaryKey(uuid.UUID, auto=True)
-#     apelido = Required(str)
-#     nome = Required(str)
-#     nascimento = Required(str)
-#     stack = Optional(StrArray)
-
-#     def to_dict(self, *args, **kwargs):
-#         """Override to return json serializable data."""
-#         data = super().to_dict(*args, **kwargs)
-#         data["id"] = str(data["id"])
-
-#         return data
-
-#     @staticmethod
-#     def create_person(*args, **kwargs):
-#         data = kwargs
-#         person = Person(
-#             apelido=data.get("apelido"),
-#             nome=data.get("nome"),
-#             nascimento=data.get("nascimento"),
-#             stack=data.get("stack"),
-#         )
-
-#         return person
-
-class Transaction:
-    __slots__ = ["valor", "tipo", "descricao", "id_do_cliente"]
-
-    def __init__(self, valor, tipo, descricao, id_do_cliente):
-        self.valor = valor
-        self.tipo = tipo
-        self.descricao = descricao
-        self.id_do_cliente = id_do_cliente
+class Transaction(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    id_do_cliente = Required("Client", index=True, reverse="transactions")
+    valor = Required(int)
+    tipo = Required(str)
+    descricao = Optional(str)
 
     def to_dict(self):
-        return {"valor": self.valor, "descricao": self.descricao, "tipo": self.tipo}
+        return {
+            "valor": self.valor,
+            "descricao": self.descricao,
+            "tipo": self.tipo,
+            "realizada_em": datetime.now().isoformat()
+        }
 
-# db.generate_mapping(create_tables=True)
-from collections import deque
-from datetime import datetime
 
-
-transactions = deque(maxlen=100_000)
-balances = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0}
+db.generate_mapping(create_tables=True)
 limits = {1: 10000, 2: 80000, 3: 1000000, 4: 10000000, 5: 500000}
+for cid, limit in limits.items():
+    try:
+        with db_session:
+            client = Client(id=cid, limit=limit, balance=0)
+    except TransactionIntegrityError:
+        with db_session:
+            for client in Client.select():
+                client.delete()
+    finally:
+        with db_session:
+            client = Client[cid]
+            client.limit = limit
+            commit()
 
 
-def ingest_transaction(transaction):
+def ingest_transaction(client, transaction):
     if transaction.tipo == "c":
-        balances[transaction.id_do_cliente] += transaction.valor
+        client.balance += transaction.valor
     elif transaction.tipo == "d":
-        balances[transaction.id_do_cliente] += transaction.valor
-
-    transactions.append(transaction)
+        client.balance -= transaction.valor
 
     return None
 
 
-def get_client_transactions(client_id):
-    return filter(lambda transaction: transaction.id_do_cliente == client_id, transactions)
+def get_client_transactions(client_id, limit=None):
+    with db_session:
+        if limit is not None:
+            return Transaction.select(id_do_cliente=client_id)[:limit - 1]
+        return Transaction.select(id_do_cliente=client_id)
 
 
 class TransactionResource:
+    @db_session
     def on_get(self, req, resp, client_id):
         try:
-            balances[client_id]
-        except KeyError:
+            client = Client[client_id]
+        except Exception:
             resp.status = falcon.HTTP_404
+            resp.media = {"status": 404, "detail": "Cliente nao encontrado"}
 
             return resp
 
         balance = {
             "saldo": {
-                "total": balances[client_id],
-                "limite": limits[client_id],
+                "total": client.balance,
+                "limite": client.limit,
                 "data_extrato": datetime.now().isoformat(),
             },
-            "ultimas_transacoes": []
+            "ultimas_transacoes": [tx.to_dict() for tx in client.last_10_transactions()]
         }
 
         resp.media = balance
 
-    # @jsonschema.validate(person_schema)
     def on_post(self, req, resp, client_id):
         try:
-            balances[client_id]
-        except KeyError:
+            with db_session:
+                client = Client[client_id]
+        except Exception:
             resp.status = falcon.HTTP_404
 
             return resp
 
         data = req.get_media()
-        transaction = Transaction(**data, id_do_cliente=client_id)
-        ingest_transaction(transaction)
+        if data["tipo"] == "d" and (abs(client.balance) + data["valor"]) > client.limit:
+            resp.status = falcon.HTTP_422
+            resp.media = {"status": 422, "detail": "Nao foi possivel adicionar a transacao"}
 
-        resp.media = {"saldo": balances[client_id], "limite": limits[client_id]}
+            return resp
+
+        try:
+            with db_session:
+                client = Client[client_id]
+                transaction = Transaction(**data, id_do_cliente=client_id)
+                ingest_transaction(client, transaction)
+        except OptimisticCheckError:
+            pass
+
+        resp.media = {"saldo": client.balance, "limite": client.limit}
         resp.status = 200
 
 
