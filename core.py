458f7d7610e2aa0892e86d69876ed869b8aa3e16
Miguel Vellasco <miguelvellasco@gmail.com>
2024-02-16 20:18:14 -0300

    ref(fiber): removed uncessary parenthesis from db_session decorator

--- core.py
+++ core.py
@@ -91,7 +91,7 @@ class BalanceResource:
     TODO: add more details here.
     """
 
-    @db_session()
+    @db_session
     def on_get(self, req, resp, client_id):
         try:
             client = Client[client_id]
